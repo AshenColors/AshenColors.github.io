c6bb22b9a7667a176069a082a5e45961efc20bf9
AshenColors <AshenColors@users.noreply.github.com>
2020-05-07 21:52:22 -0700

    Initial Leaflet setup with mostly blank map

---
+++ data/Towns_3.js
@@ -0,0 +1,8 @@
+var json_Towns_3 = {
+"type": "FeatureCollection",
+"name": "Towns_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Oberwerth", "Wealth": 1.0, "Peoples": "Himmelvolk" }, "geometry": { "type": "Point", "coordinates": [ 51.309242236732899, 10.201671213144627 ] } }
+]
+}
